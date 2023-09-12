c97889d634ba8459fcfbf77f74857db28f736ee9
Louis469 <louis@kripke.me>
2023-09-12 12:45:25 +0930

    fixed

--- Main/main.py
+++ Main/main.py
@@ -8,7 +8,7 @@ def send_message():
     user_message = user_input.get()
     if user_message:
         add_message("User:", user_message)
-        output = model.generate(user_message, max_tokens=1000)
+        output = model.generate("Question: " + user_message, max_tokens=1000)
         add_message("ChatGPT:", output)
 
 
